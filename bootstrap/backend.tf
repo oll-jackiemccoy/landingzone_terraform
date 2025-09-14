b9ba40be364420afeccea34b78ac7a71d3b8311d
APKAYRVWGRRJSRN7KMB3 <jackie@snowball.localdomain>
2025-09-14 14:29:57 -0400

    Committer: jackie <jackie@snowball.localdomain> updating backend

--- bootstrap/backend.tf
+++ bootstrap/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraformollionstatebucket"
+    bucket         = "terraform-ollionstatebucket"
     key            = "control-tower/bootstrap.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
